1bab5a1f69c3168598442bdbfd859df2f729f5d9
Ahmed Calif <ahmedcalif88@gmail.com>
2025-04-28 09:32:27 -0700

    initial dotfiles commit

---
+++ .config/i3/lockscreen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Simple screen lock
+#i3lock -c 000000
+
+# Or for a more fancy version with blur effect:
+ scrot /home/ahmed/Pictures/got_bw.png
+# convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+ i3lock -i /home/ahmed/Pictures/got_bw.png
